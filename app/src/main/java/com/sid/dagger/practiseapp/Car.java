10b0a13c97a9c12f5225004d8e45c3e803792646
kavitasid <kkavitasingh95@gmail.com>
2020-05-02 11:52:43 +0800

    Initial dagger commit

---
+++ app/src/main/java/com/sid/dagger/practiseapp/Car.java
@@ -0,0 +1,21 @@
+package com.sid.dagger.practiseapp;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Car {
+    private static final String TAG = "Car";
+    private Engine mEngine;
+    private Wheels mWheels;
+
+    @Inject
+    public Car(Engine mEngine, Wheels mWheels) {
+        this.mEngine = mEngine;
+        this.mWheels = mWheels;
+    }
+
+    public void drive(){
+        Log.d(TAG, "drive: driving---");
+    }
+}
---
+++ app/src/main/java/com/sid/dagger/practiseapp/CarComponent.java
@@ -0,0 +1,9 @@
+package com.sid.dagger.practiseapp;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+
+    Car getCar();
+}
